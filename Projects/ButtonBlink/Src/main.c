568359ed27d6df9c269306af998647d31198d6e9
Danyal Rashid <drashid@pop-os.localdomain>
2021-01-20 17:15:50 -0800

    Structural Changes

--- Projects/ButtonBlink/Src/main.c
+++ Projects/ButtonBlink/Src/main.c
@@ -4,39 +4,36 @@
 
 void delay(void)
 {
-	for(int i = 0; i < 50000 i++)
+	for(int i = 0; i < 50000; i++);
 }
 
 
+GPIO_Config_t GPIOLED, GPIOBTN;
 void GPIO_Init(void)
 {
-	GPIO_Config_t GPIOLED, GPIOBTN;
 
-	//Button Init
+
+	//Button GPIO Init
 	GPIOBTN.pGPIOx = GPIOB;
 	GPIOBTN.GPIO_PinNumber = GPIO_PIN_5;
 	GPIOBTN.GPIO_Mode = GPIO_MODE_IN;
 	GPIOBTN.GPIO_Speed = GPIO_SPEED_FAST;
 	GPIO_PeriClockControl(&GPIOBTN, ENABLE);
-	
-}
+	GPIO_Init(&GPIOBTN);
 
-int main(void)
-{
-	GPIO_Config_t GPIOLED,GPIOBTN;
-	
+	// LED GPIO Init
 	GPIOLED.pGPIOx = GPIOA;
-	
 	GPIOLED.GPIO_PinNumber = GPIO_PIN_12;
-	
 	GPIOLED.GPIO_Mode = GPIO_MODE_OUT;
 	GPIOLED.GPIO_OutputType = GPIO_OT_PP;
 	GPIOLED.GPIO_PUPD = GPIO_NO_RESISTOR;
-	
-	GPIOLED.GPIO_Speed = GPIO_SPEED_FAST;
-	GPIO_PeriClockControl(&GPIOLED, ENABLE);
-	GPIO_Init(&GPIOBTN);
-	GPIO_Init(&GPIOLED);
+	GPIOLED.GPIO_Speed = GPIO_SPEED_FAST;	
+	GPIO_Init(&GPIOLED);	
+}
+
+int main(void)
+{	
+	GPIO_Init();
 	
 	while(1)
 	{
