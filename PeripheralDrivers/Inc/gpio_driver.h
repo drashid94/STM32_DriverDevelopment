676e2aaf03085f5bb5f26f0c6a52210f4b26a5ba
Danyal Rashid <70240202+drashid94@users.noreply.github.com>
2021-05-06 21:44:02 -0700

    Update gpio_driver.h

--- PeripheralDrivers/Inc/gpio_driver.h
+++ PeripheralDrivers/Inc/gpio_driver.h
@@ -13,8 +13,8 @@
 #define GPIO_MODE_IT_RFT		6
 
 //Possible GPIOA Output Types
-#define GPIO_OT_PP					0
-#define GPIO_OT_OD					1	
+#define GPIO_OT_PP			0
+#define GPIO_OT_OD			1	
 
 //Possible GPIO Output Speeds
 #define GPIO_SPEED_LOW 			0
@@ -24,48 +24,48 @@
 
 //GPIO Pull UP or Pull DOWN
 #define GPIO_NO_RESISTOR		0
-#define GPIO_PULLUP 				1
+#define GPIO_PULLUP 			1
 #define GPIO_PULLDOWN 			2
 
 //GPIO Pin Numbers
-#define GPIO_PIN_0 					0
-#define GPIO_PIN_1 					1
-#define GPIO_PIN_2 					2
-#define GPIO_PIN_3 					3
-#define GPIO_PIN_4 					4
-#define GPIO_PIN_5 					5
-#define GPIO_PIN_6 					6
-#define GPIO_PIN_7 					7
-#define GPIO_PIN_8 					8
-#define GPIO_PIN_9 					9
-#define GPIO_PIN_10 				10
-#define GPIO_PIN_11 				11
-#define GPIO_PIN_12 				12
-#define GPIO_PIN_13 				13
-#define GPIO_PIN_14 				14
-#define GPIO_PIN_15 				15
+#define GPIO_PIN_0 			0
+#define GPIO_PIN_1 			1
+#define GPIO_PIN_2 			2
+#define GPIO_PIN_3 			3
+#define GPIO_PIN_4 			4
+#define GPIO_PIN_5 			5
+#define GPIO_PIN_6 			6
+#define GPIO_PIN_7 			7
+#define GPIO_PIN_8 			8
+#define GPIO_PIN_9 			9
+#define GPIO_PIN_10 			10
+#define GPIO_PIN_11 			11
+#define GPIO_PIN_12 			12
+#define GPIO_PIN_13 			13
+#define GPIO_PIN_14 			14
+#define GPIO_PIN_15 			15
 
 //GPIO Alternate Function Modes
-#define AF0	 								0
-#define AF1	 								1
-#define AF2	 								2
-#define AF3	 								3
-#define AF4	 								4
-#define AF5 								5
-#define AF6	 								6
-#define AF7 								7
-#define AF8	 								8
-#define AF9 								9
-#define AF10 								10
-#define AF11 								11
-#define AF12 								12
-#define AF13 								13
-#define AF14 								14
-#define AF15 								15
+#define AF0	 			0
+#define AF1	 			1
+#define AF2	 			2
+#define AF3	 			3
+#define AF4	 			4
+#define AF5 				5
+#define AF6	 			6
+#define AF7 				7
+#define AF8	 			8
+#define AF9 				9
+#define AF10 				10
+#define AF11 				11
+#define AF12 				12
+#define AF13 				13
+#define AF14 				14
+#define AF15 				15
 
 //Other Useful Macros
-#define GPIO_LOW				0
-#define GPIO_HIGH				1
+#define GPIO_LOW			0
+#define GPIO_HIGH			1
 
 typedef struct
 {
